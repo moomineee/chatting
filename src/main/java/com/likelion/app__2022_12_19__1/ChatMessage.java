3a5a686fb4e1c93ff421b35071509a921df6d92c
momineee <p.rainmakerm@gmail.com>
2022-12-19 10:37:29 +0900

    채팅메세지 리스트에 저장

---
+++ src/main/java/com/likelion/app__2022_12_19__1/ChatMessage.java
@@ -0,0 +1,27 @@
+package com.likelion.app__2022_12_19__1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ChatMessage {
+    private long id;
+    private LocalDateTime createDate;
+    private String authorName;
+    private String content;
+
+    public ChatMessage(String authorName, String content) {
+        this(ChatMessageIdGenerator.genNextId(), LocalDateTime.now(), authorName, content);
+    }
+}
+
+class ChatMessageIdGenerator {
+    private static long id = 0;
+
+    public static long genNextId() {
+        return ++id;
+    }
+}
---
+++ src/main/java/com/likelion/app__2022_12_19__1/RsData.java
@@ -0,0 +1,12 @@
+package com.likelion.app__2022_12_19__1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RsData<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
+}
